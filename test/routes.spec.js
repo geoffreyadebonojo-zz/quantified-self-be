b4a8760dea024eb6f150c9d35bc2ba4b6bb9ae6c
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-30 10:50:55 -0700

    Create test for foods/:id

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../index');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -12,6 +16,7 @@ describe('Client Routes', () => {
     .end((err, response) => {
       response.should.have.status(200);
       response.should.be.json;
+      response.body.should.be.a('array');
       done();
     });
   });
@@ -25,8 +30,70 @@ describe('Client Routes', () => {
     })
   });
 
+  it('should return a food by id', function(done) {
+    chai.request(server)
+    .get('/api/v1/foods/3')
+    .end((err, response) =>
+    {
+      response.should.be.json;
+      response.body[0].should.be.a('object');
+      response.body[0].should.have.property('name');
+      response.body[0].should.have.property('calories');
+      done();
+    })
+  });
+
 });
 
 describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
 
+  // beforeEach((done) => {
+  //   database.seed.run()
+  //     .then(() => done())
+  //     .catch(error => {
+  //       throw error;
+  //     });
+  // });
+
+  // after((done) => {
+    // database teardown
+  // });
+
+  xdescribe('GET /api/v1/foods', () => {
+    it('should return all of the foods', done => {
+      chai.request(server)
+      .get('/api/v1/foods')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.be.a('array')
+        done();
+      });
+    });
+  });
+
+  describe('POST /api/v1/foods', () => {
+    it('should create a new food', done => {
+      chai.request(server)
+      .post('/api/v1/foods')
+      .send({
+        name: "Cheesecake",
+        calories: "500"
+      })
+      .end((err, response) => {
+        response.should.have.status(201)
+        response.should.be.json
+        response.body.should.be.a('object');
+        response.body.should.have.property('food');
+        done();
+      });
+    });
+  });
 });

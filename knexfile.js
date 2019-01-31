dae56e492502a915fc767251a3c5ceb62e13a16d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-31 15:03:12 -0700

    update knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: 'postgres:/postgres://yxovmbfskfuvso:1a71ca6f93c0f3eb8bd886fb10db0866f4c3bc3202f9cf013bac2baa09912c68@ec2-174-129-224-157.compute-1.amazonaws.com:5432/d73oimguek2kqf',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    ssl: true
   }
 };
